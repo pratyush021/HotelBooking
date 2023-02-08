dd2edb8447bda9f637720d27261be7d8dc81d058
Pratyush Ranjan Bohidar <probohidar2016@gmail.com>
2023-02-08 15:02:42 +0530

    fixed the class path issue

--- hotelbooking/src/main/java/com/hotelbooking/HotelbookingApplication.java
+++ hotelbooking/src/main/java/com/hotelbooking/HotelbookingApplication.java
@@ -2,8 +2,10 @@ package com.hotelbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HotelbookingApplication {
 
 	public static void main(String[] args) {
--- hotelbooking/src/main/java/com/hotelbooking/controller/UserController.java
+++ hotelbooking/src/main/java/com/hotelbooking/controller/UserController.java
@@ -2,24 +2,44 @@ package com.hotelbooking.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotelbooking.HotelbookingApplication;
 import com.hotelbooking.entity.User;
+import com.hotelbooking.feign.AuthFeign;
 import com.hotelbooking.service.UserService;
 
+import ch.qos.logback.classic.Logger;
+
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
 	
 	@Autowired
 	private UserService userService;
+	
+	
+	@Autowired
+	private AuthFeign auth; 
+	
+	private static final Logger log = (Logger) LoggerFactory.getLogger(HotelbookingApplication.class); 
+	
+	@GetMapping("/health")
+	public String healthCheck(@RequestHeader(name = "Authorization", required=true) String token ) throws Exception  {
+		
+		return token;
+	}
 	
 	
 	@GetMapping("/user")
@@ -49,5 +69,6 @@ public class UserController {
 		
 		return ResponseEntity.ok("user added"); 
 				
+		
 	}
 }
